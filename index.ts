4d4f04c5f197d8b322a9ac49a627a1252df91d25
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2022-10-26 00:03:13 +0530

    Fix null response

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 // Proxy
-const host_url = "https://small-chicken-97.deno.dev"
-const target_url = "https://www.1337x.com"
+const host_name  = "small-chicken-97.deno.dev";
+const target_name =  "1337x.to"
+
+const host_url = "https://" + host_name;
+const target_url = "https://" + target_name;
+
 
 async function handler(_req: Request): Response {
     // replace url
-    var url = new URL(_req.url.replace(host_url, target_url));
+    var url = new URL(_req.url.replace(host_name, target_name));
     console.log("Fetching url: " + url);
     const r = await fetch(url, {
         // headers:
@@ -14,11 +18,19 @@ async function handler(_req: Request): Response {
         method: _req.method,
         redirect: _req.redirect,
     });
+
+    // Log
+    console.log("Response: [" + r.status + "]");
+    
     // Return
     var res = await r.text();
-    res = res.replace(target_url, host_url);
+
+    // Check null and replace
+    res = res === null? '': res.replace(target_name, host_name);
+    res = await removeAds(res);
+    // Response
     return new Response(res, {
-        status: r.status,
+        status: r.status === null? 200: r.status,
         headers: r.headers
     });
 }
